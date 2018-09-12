b5794461150ec3b2d36f572ba055a38666415f63
uuuno <uuu0829@gmail.com>
2018-09-12 23:49:24 +0900

    daily

---
+++ vue/vue-webpack-app/src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Hello, World!</p>
+	<counter/>
+	<avatar username="Jane Doe"></avatar>
+    </div>
+</template>
+
+<script>
+import Counter from "./Counter"
+import Avatar from "vue-avatar"
+
+export default{
+       components: {Counter, Avatar},
+}
+</script>
+
+<style>
+p{
+	font-size: 20px;
+}
+</style>
